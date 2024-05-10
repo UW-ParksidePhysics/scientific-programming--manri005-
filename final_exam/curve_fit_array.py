ba225460c6e7dca9b1812811bb083299593ac7ec
manri005 <98779083+manri005@users.noreply.github.com>
2024-05-10 10:23:44 -0500

    Update curve_fit_array.py

--- final_exam/curve_fit_array.py
+++ final_exam/curve_fit_array.py
@@ -5,9 +5,11 @@ This code computes and prints out the x and y values in an array using the quadr
 __author__ =  'Olivia Manring' 
 
 import numpy as np
+from calculate_quadratic_fit import calculate_quadratic_fit
+from calculate_bivariate_statistics import calculate_bivariate_statistics
+from read_two_column_text import read_two_column_text
 
-
-def fit_curve_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
+def curve_fit_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
     """
     This returns x and y values which the y values are computed by the quadratic equation
     :param quadratic_coefficients: shape(3,)
@@ -30,4 +32,5 @@ def fit_curve_array(quadratic_coefficients, min_x, max_x, number_of_points=100):
 
 if __name__ == "__main__":
     test_coefficients = [0,0,1]
+    print(curve_fit_array(test_coefficients, -2, 2, number_of_points=7)),0,1]
     print(fit_curve_array(test_coefficients, -2, 2, number_of_points=7))
