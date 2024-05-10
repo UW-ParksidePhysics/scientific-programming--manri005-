892744cada4e07864429b5b829ddc6b9574c3519
manri005 <98779083+manri005@users.noreply.github.com>
2024-05-10 10:22:09 -0500

    Update calculate_bivariate_statistics.py

--- final_exam/calculate_bivariate_statistics.py
+++ final_exam/calculate_bivariate_statistics.py
@@ -11,23 +11,28 @@ from scipy import stats
 sample_x = np.linspace(-10, 10, 22)
 sample_y = sample_x ** 2
 
-def compute_bivariate_statistics(data):
-        """
-        Compute statistics from provided data.
-
-        :param data: ndarray, shape(2,M)
-        :return: statistics: ndarray, shape(6,)
-        """
-        try:
-            stats_x = stats.describe(data[0])
-            stats_y = stats.describe(data[1])
-            statistics = np.array([stats_y.mean, np.sqrt(stats_y.variance), stats_x.minmax[0], stats_x.minmax[1], stats_y.minmax[0], stats_y.minmax[1]])
-        except IndexError as e:
-            print(f'Error: {e}')
+def calculate_bivariate_statistics(data):
+    try:
+        stats_x = stats.describe(data[0])
+        stats_y = stats.describe(data[1])
+        statistics = np.array([stats_y.mean, np.sqrt(stats_y.variance), stats_x.minmax[0], stats_x.minmax[1], stats_y.minmax[0], stats_y.minmax[1]])
         return statistics
+    except IndexError as e:
+        print(f'Error: {e}')
 
 if __name__ == "__main__":
-        # Expected values
+    # Expected values
+    expected_values = [np.mean(sample_y), np.sqrt(np.var(sample_y)), np.min(sample_x), np.max(sample_x), np.min(sample_y), np.max(sample_y)]
+
+    # Statistical names
+    stat_names = ['mean of y', 'std of y', 'min of x', 'max of x', 'min of y', 'max of y']
+
+    print('name:expected:value')
+    # Display computed statistics
+    for name, expected, value in zip(stat_names, expected_values, calculate_bivariate_statistics([sample_x, sample_y])):
+        print(f'{name}: {expected}, {value.mean}, {value.variance}, {value.minmax[0]}, {value.minmax[1]}')
+
+s
         expected_values = [np.mean(sample_y), np.sqrt(np.var(sample_y)), np.min(sample_x), np.max(sample_x), np.min(sample_y), np.max(sample_y)]
 
         # Statistical names
